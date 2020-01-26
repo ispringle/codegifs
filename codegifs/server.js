1d86ba6917f6f6ca4d63ba5db780df2a9a24b44c
Ian Pringle <ipringle2@liberty.edu>
2020-01-25 20:56:32 -0500

    Sorta got the look down right! 👍

--- codegifs/server.js
+++ codegifs/server.js
@@ -18,6 +18,54 @@ app.get("/", function(req, res) {
   res.sendFile("index.html", { root: __dirname });
 });
 
+// Add the WebSocket handlers
+io.on("connection", function(socket) {
+  console.log(`a user has connected on socket ${socket.id}`);
+});
+
+function hashSocket(sock) {
+  var hash = 0;
+  for (var i = 0; i < sock.length; i++) {
+    hash = sock.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+}
+
+function intToRGB(i) {
+  var c = (i & 0x00ffffff).toString(16).toUpperCase();
+  return "00000".substring(0, 6 - c.length) + c;
+}
+
+var players = {};
+io.on("connection", function(socket) {
+  socket.on("new player", function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300,
+      color: `#${intToRGB(hashSocket(socket.id))}`
+    };
+  });
+  socket.on("movement", function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
+});
+
+setInterval(function() {
+  io.sockets.emit("state", players);
+}, 1000 / 60);
+
 function getGIF() {
   return new Promise((resolve, rej) => {
     const api_url =
